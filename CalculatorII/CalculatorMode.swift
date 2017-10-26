7bb7f501f5d1fc28f48517fd18d4ae49312f1d61
Jeff Ripke <jripke74@gmail.com>
2017-10-26 08:36:39 -0700

    add enum for calculator modes -, +, / etc..

---
+++ CalculatorII/CalculatorMode.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorMode.swift
+//  CalculatorII
+//
+//  Created by Jeff Ripke on 10/26/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+enum CalculatorMode {
+    case notSet
+    case addition
+    case subtraction
+    case mutliplication
+    case division
+}
